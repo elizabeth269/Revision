477b74a6cf24edfc34421b0347d3637329934497
Elizbeth <elizabethadebayo269@gmail.com>
2024-07-29 17:06:17 +0100

    printing stars

--- revise.py
+++ revise.py
@@ -1,1 +1,9 @@
-print('hello world')
+star = "*"
+star + star
+# print(star * 20)
+# print(star,"                ", star)
+# print(star,"                ", star)
+# print(star * 20)
+if star < 4*star:
+    print (star)
+   
