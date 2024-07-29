c1d7e6b7e855ffbf70b4b51b8023f9e06b5ec33c
Elizbeth <elizabethadebayo269@gmail.com>
2024-07-29 22:48:19 +0100

    a dgnago roject

--- revise.py
+++
@@ -1,9 +0,0 @@
-star = "*"
-star + star
-# print(star * 20)
-# print(star,"                ", star)
-# print(star,"                ", star)
-# print(star * 20)
-if star < 4*star:
-    print (star)
-   
