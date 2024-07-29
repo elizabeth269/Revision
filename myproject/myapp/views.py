c296e98e7cdd091dbfc9ff5047b58e755cfc046b
Elizbeth <elizabethadebayo269@gmail.com>
2024-07-30 00:05:38 +0100

    rendering dynamic files

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
+    # context = {
+    #     'name': "elizabeth",
+    #     'age': 23,
+    #     'nationality': "nigerian",
+    # }
     return render(request, 'index.html')
+
+def counter(request):
+    
+    return render(request, "counter.html")
